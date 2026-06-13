3166710cee78154aece4bfe1b36c2ebf847ad62c
VernaculusF <fizikg@list.ru>
2026-06-13 14:53:51 +0500

    del shit

--- check_path.py
+++
@@ -1,4 +0,0 @@
-import sonya
-import sys
-print(sonya.__file__)
-print(sys.path)
--- query_models.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('/home/jester-sonya/Sonya/sonya.db')
-cursor = conn.cursor()
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-for row in cursor.fetchall():
-    print(row[0])
